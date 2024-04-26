478df0821cf72376da32fc723b5536edd4b1ae6a
rodrigo <rodrigo.alonso.pereira@gmail.com>
2024-04-26 15:24:14 -0400

    Implementacion de get's para TDA train

--- TDA_train.rkt
+++ TDA_train.rkt
@@ -2,6 +2,56 @@
 
 (provide (all-defined-out))
 
+;; Funcion que entrega un 'id' de un tren
+
+; Dom: train (train)
+; Rec: id (int)
+
+(define train-get-id
+  (lambda (train)
+    (first train)))
+
+
+;; Funcion que entrega un 'maker' de un tren
+
+; Dom: train (train)
+; Rec: maker (string)
+
+(define train-get-maker
+  (lambda (train)
+    (second train)))
+
+
+;; Funcion que entrega un 'rail-type' de un tren
+
+; Dom: train (train)
+; Rec: rail-type (string)
+
+(define train-get-rail-type
+  (lambda (train)
+    (third train)))
+
+
+;; Funcion que entrega un 'speed' de un tren
+
+; Dom: train (train)
+; Rec: speed (positive number)
+
+(define train-get-speed
+  (lambda (train)
+    (fourth train)))
+
+
+;; Funcion que entrega un 'station-stay-time' de un tren
+
+; Dom: train (train)
+; Rec: station-stay-time (positive number U {0})
+
+(define train-get-station-stay-time
+  (lambda (train)
+    (fifth train)))
+
+
 ;; Funcion que entrega una lista de 'pcar' de un tren
 
 ; Dom: train (train)
