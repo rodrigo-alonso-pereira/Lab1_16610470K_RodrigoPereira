300f4825f86b18d4a816f536464c28035f1fd69f
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-04-27 22:24:59 -0400

    Implementacion pcar-get-model en TDA_train

--- TDA_pcar.rkt
+++ TDA_pcar.rkt
@@ -2,6 +2,15 @@
 
 (provide (all-defined-out))
 
+;; Funcion que entrega un 'model' de un pcar
+
+; Dom: pcar (pcar)
+; Rec: model (string) 
+
+(define pcar-get-model
+  (lambda (pcar)
+    (third pcar)))
+
 ;; Funcion que entrega un 'car-type' de un pcar
 
 ; Dom: pcar (pcar)
