d69f6379f5c8525c2f0f806f0cf6f75747d18c0f
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-04-27 23:27:47 -0400

    Implementacion pcar-get-capacity en TDA_train

--- TDA_pcar.rkt
+++ TDA_pcar.rkt
@@ -2,6 +2,16 @@
 
 (provide (all-defined-out))
 
+;; Funcion que entrega un 'capacity' de un pcar
+
+; Dom: pcar (pcar)
+; Rec: capacity (positive integer) 
+
+(define pcar-get-capacity
+  (lambda (pcar)
+    (second pcar)))
+
+
 ;; Funcion que entrega un 'model' de un pcar
 
 ; Dom: pcar (pcar)
@@ -10,6 +20,7 @@
 (define pcar-get-model
   (lambda (pcar)
     (third pcar)))
+
 
 ;; Funcion que entrega un 'car-type' de un pcar
 
