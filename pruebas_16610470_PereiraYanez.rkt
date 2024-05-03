e883d93518c142a60095df332451f9e639f73498
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-02 20:15:19 -0400

    Se agrega script de pruebas

---
+++ pruebas_16610470_PereiraYanez.rkt
@@ -0,0 +1,86 @@
+#lang racket
+
+(require "tdaStation_16610470_PereiraYanez.rkt")
+(require "tdaLine_16610470_PereiraYanez.rkt")
+(require "tdaSection_16610470_PereiraYanez.rkt")
+(require "tdaPcar_16610470_PereiraYanez.rkt")
+(require "tdaTrain_16610470_PereiraYanez.rkt")
+(require "tdaSubway_16610470_PereiraYanez.rkt")
+(require "main _16610470_PereiraYanez.rkt")
+
+;Creando estaciones L1 simplificada metro santiago
+(define e0 (station 0 "San Pablo" t 90))
+(define e1 (station 1 "Neptuno" r 45))
+(define e2 (station 2 "Pajaritos" c 45))
+(define e3 (station 3 "Las Rejas" r 45))
+(define e4 (station 4 "Ecuador" r 60))
+(define e5 (station 5 "San Alberto Hurtado" r 40))
+(define e6 (station 6 "Universidad de Santiago de Chile" c 40))
+(define e7 (station 7 "Estación Central" c 45))
+(define e8 (station 8 "Unión Latinoamericana" r 30))
+(define e9 (station 9 "República" r 40))
+(define e10 (station 10 "Los Héroes" c 60))
+(define e11 (station 11 "La Moneda" r 40))
+(define e12 (station 12 "Universidad de Chile" c 90))
+(define e13 (station 13 "Santa Lucía" r 40))
+(define e14 (station 14 "Universidad Católica" c 60))
+(define e15 (station 15 "Baquedano" r 40))
+(define e16 (station 16 "Los Dominicos" t 90))
+(define e17 (station 17 "Cochera Neptuno" m 3600))
+
+;Creando estaciones L2 simplificada metro santiago, para una versión circular
+(define e18 (station 18 "El Llano" r 60))
+(define e19 (station 19 "Franklin" r 50))
+(define e20 (station 20 "Rondizzoni" r 55))
+(define e21 (station 21 "Parque O'Higgins" r 65))
+(define e22 (station 22 "Toesca" r 65))
+(define e23 (station 23 "Santa Ana" c 65))
+(define e24 (station 24 "Puente Cal y Canto" r 65))
+
+; Ejemplos
+
+
+;Creando tramos Línea 1
+(define s0 (section e0 e1 4 15))
+(define s1 (section e1 e2 3 14))
+(define s2 (section e2 e3 2.5 10))
+(define s3 (section e3 e4 4.5 17))
+(define s4 (section e4 e5 4.7 18))
+(define s5 (section e5 e6 4.3 17))
+(define s6 (section e6 e7 3.8 12))
+(define s7 (section e7 e8 2.5 10))
+(define s8 (section e8 e9 4.5 17))
+(define s9 (section e9 e10 4.7 18))
+(define s10 (section e10 e11 4.3 17))
+(define s11 (section  e11 e12 3.8 12))
+(define s12 (section e12 e13 4.5 17))
+(define s13 (section e13 e14 4.7 18))
+(define s14 (section e14 e15 4.3 17))
+(define s15 (section e15 e16 4.2 17))
+;enlace cochera
+(define s16 (section e1 e17 3.8 12))
+
+;Creando tramos Línea 2, línea circular
+(define s17 (section e18 e19 4 15))
+(define s18 (section e19 e20 3 12))
+(define s19 (section e20 e21 5 18))
+(define s20 (section e21 e22 4.5 16))
+(define s21 (section e22 e10 4.2 16))
+(define s22 (section e10 e23 4.2 16))
+(define s23 (section e23 e24 4.2 16))
+(define s24 (section e24 e18 28 90))
+
+;creación de Línea 1 con todos los tramos
+(define l1 (line 1 "Línea 1" "UIC 60 ASCE" s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 s12 s13 s14 s15))
+;creación de Línea 2 sin incluir tramos
+(define l2 (line 2 "Línea 2" "100 R.E."))
+
+;creación de Línea 1 con todos los tramos
+(define l1 (line 1 "Línea 1" "UIC 60 ASCE" s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 s12 s13 s14 s15))
+;creación de Línea 2 sin incluir tramos
+(define l2 (line 2 "Línea 2" "100 R.E."))
+
+;obteniendo distancia total de linea
+(line-lenght l1) ;resultado debe ser 64,3 si considera inclusive los tramos hacia estaciones de mantenimiento 
+(line-lenght l2) ;resultado debe ser 0
+
